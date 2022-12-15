f36f7f6980aa40a72bca61dc7e7d715af1e4e5e7
Zhanna <zhannaburlaka@gmail.com>
2022-12-15 19:23:36 +0100

    Lesson_28_updated

--- src/Homework_1.java
+++ src/Homework_1.java
@@ -1,3 +1,69 @@
 public class Homework_1 {
-}
     public static void main(String[] args) {
+        int current_year = 2022;
+        System.out.println(current_year);
+        int friend_Year = 1997;
+        System.out.println(friend_Year);
+        System.out.println("My friend has 25 years old");
+
+        int my_number = 1993022;
+        System.out.println(my_number);
+
+        int last_Digit = 2;
+        System.out.println(last_Digit);
+
+        int age = 26 ;
+        if (age>=18) ;
+        if (age<=31) ;
+        System.out.println("Жанна, поздравляю вы разобрались как работает if");
+
+        int x= 0;
+        int y= 0;
+
+        if (x >0) ;
+        if (y >0) ;
+        System.out.println("It is the I quarter") ;
+
+        if (x<0) ;
+        if (y>0) ;
+        System.out.println("It is the II quarter") ;
+
+        if (x<0) ;
+        if (y<0) ;
+        System.out.println("It is the III quarter") ;
+
+        if (x>0) ;
+        if (y<0) ;
+        System.out.println("It is the IV quarter") ;
+
+        if (x==0) ;
+        System.out.println("Цель находится на оси x") ;
+
+
+        if (y==0) ;
+        System.out.println("Цель находится на оси y") ;
+
+
+        int a= 2;
+        int b= 2;
+        int c= 3;
+
+        if (a+b>c)
+        if (b+c>a)
+        if (a+c>b)
+
+        {
+        System.out.println("Такой треугольник можно построить");
+        }
+
+        else {
+            System.out.println ("Такой треугольник нельзя построить");
+        }
+
+
+
+
+
+
+    }
+}
