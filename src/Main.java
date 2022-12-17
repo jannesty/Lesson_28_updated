00ee3f28a886ccc69ee88c15a119c68bf4ab6645
Zhanna <zhannaburlaka@gmail.com>
2022-12-17 11:42:42 +0100

    Lesson_28_updated

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //System.out.println("Привет Жанна!");
@@ -40,7 +42,7 @@ public class Main {
         System.out.println (a==b) ;
         System.out.println (a!=b) ; */
 
-        int age = 17 ;
+        /*int age = 17 ;
         if (age>=18) {
             System.out.println("You are an old person");
         } else {
@@ -76,6 +78,68 @@ public class Main {
                 break;
             default:
                 System.out.println ("No such day") ;
-        }
+        } */
+
+        /*int k = 0 ;
+        while (k<5) {
+            System.out.println (k);
+            k++ ;
+        }*/
+
+        /*int i_love_ukraine = 0 ;
+        while (i_love_ukraine<17) {
+            System.out.println (i_love_ukraine) ;
+            i_love_ukraine ++ ;
+        }*/
+
+        /*String s= console.nextLine() ;
+
+        System.out.println ("My string" +s) ; */
+
+       /* while (true) {
+            String s_new = console.nextLine();
+            if (s_new.equals("exit")) {
+                break;
+            }
+        }*/
+
+        /*int i = 5;
+        do {
+            System.out.println (i) ;
+            i--;
+        } while (i>0) ;*/
+
+       /* int o = 17;
+        do {
+            System.out.println (o) ;
+            o--;
+        } while (o>0) ;*/
+
+        /*int result = 0 ;
+        for (int i = 1; i <= 5; i++) {
+            if ((i%2)==0) {
+                System.out.println(i);
+                continue;
+            }
+                result = result +i ;
+        }*/
+
+      /*  int result = 0 ;
+        for (int p = 1; p <= 50; p++) {
+            if ((p%5)==0) {
+                System.out.println(p);
+                continue;
+            }
+        } */
+
+        Scanner console = new Scanner(System.in);
+        String s = console.nextLine();
+        int s_1= Integer.parseInt(s) ;
+
+        System.out.println(s+1);
+        System.out.println(s_1+1);
+
+
+
     }
 }
