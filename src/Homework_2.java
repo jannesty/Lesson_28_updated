850f4431a97e80e79422e6ba8a32dde520362334
Zhanna <zhannaburlaka@gmail.com>
2022-12-23 17:14:20 +0100

    Lesson_28_updated

---
+++ src/Homework_2.java
@@ -0,0 +1,39 @@
+
+public class Homework_2 {
+
+}
+
+    /*public static void main(String[] args){
+                System.out.println (sumDigitsInNumber (565)) ;
+        }
+        public static int sumDigitsInNumber(int number) {
+               return number / 100 + number % 100 / 10 + number % 100 % 10;
+       }
+} */
+
+/*public static void main(String[] args) {
+                int number = 6;
+                System.out.println(" Факториал " + number + " : " + factorial(number));
+        }
+
+        public static int factorial(int number) {
+                int fact = 1;
+                for (int i = 1; i <= number; i++) {
+                        fact *= i;
+                }
+
+                return fact;
+
+        }
+} */
+
+        /*public static void main(String[] args) {
+                for (int i = 1; i <= 9; i++) {
+                        int sum = 0;
+                        sum += 3 * i;
+                        System.out.print(3 + "*" + i + "=" + sum + "\t");
+                }
+                System.out.println();
+
+        }
+} /*
